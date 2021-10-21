72fa12d685a89336c0ee6fecb05e4beedd5d1b20
LipatoS <sasharubaiko@gmail.com>
2021-10-21 23:34:22 +0300

    comments

--- Alex.py
+++ Alex.py
@@ -5,3 +5,24 @@ for i in range(10):
 # ЖОПА
 
 print("hello")
+
+# переход в ветку
+# git checkout main
+
+# обновить проекст с основной ветки "main"
+# git pull origin main
+
+# создать ветку "hw"
+# git branch hw
+
+# перейти в ветку "hw"
+# git checkout hw
+
+# добавить все файлы гит
+# git add --all
+
+# создать комментарий к изменениям
+# git commit -m "task 1"
+
+# залить в гитхаб
+# git push -u origin hw
